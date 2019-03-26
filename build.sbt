2026255c4f2c6c71415f56e93914bb3baa1bdded
kuba.jal <9532517+kubajal@users.noreply.github.com>
2019-03-26 15:51:47 +0100

    added first files

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "spark-test"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+
+val sparkVersion = "2.4.0"
+// https://mvnrepository.com/artifact/org.scala-sbt/compiler-bridge
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
---
+++ src/main/scala/main/sparktest/SimpleApp.scala
@@ -0,0 +1,27 @@
+package main.sparktest
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/usr/lib/spark/README.md" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Spark test")
+    val sc = new SparkContext(conf)
+    val sparkSession = SparkSession
+      .builder()
+      .appName("spark-test")
+      .getOrCreate()
+
+    val df = sparkSession
+      .read
+      .format("csv")
+      .option("header", "true")
+      .option("mode", "DROPMALFORMED")
+      .load("/home/kuba_jal/test_data.csv")
+
+    val numFLs = df.filter(c => c.get(2) == "FL").count()
+    println("Lines with a: %s".format(numFLs))
+  }
+}
